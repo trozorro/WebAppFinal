20e61bf569d7d7573be2bd637f9e5f375820a4ef
Satyajit Pradhan <satyajiit0@gmail.com>
2019-02-12 19:26:21 +0530

    major update

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SatyaJit
+ * Date: 09-02-2019
+ * Time: 19:43
+ */
+
+
+//Contains all the values and names,
+
+$WebName="Trozorro";
+$FbLink="#";
+$GitLink="#";
+$LinkedLink="#";
+
+?>
