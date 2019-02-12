828d5085bc35f5680c527f5909d23ea35dffc893
Satyajit Pradhan <satyajiit0@gmail.com>
2019-02-13 01:51:34 +0530

    Major Update

--- header.php
+++ header.php
@@ -41,6 +41,11 @@ echo "<!DOCTYPE html>
 
 <link rel=\"stylesheet\" type=\"text/css\" href=\"/assets/css/colors/indigo.css\" media=\"screen\" />
 
+
+<link rel=\"apple-touch-icon\" sizes=\"180x180\" href=\"favicon_180.png\">
+<link rel=\"icon\" type=\"image/png\" sizes=\"32x32\" href=\"favicon-32x32.png\">
+<link rel=\"icon\" type=\"image/png\" sizes=\"16x16\" href=\"favicon-16x16.png\">
+
 </head>
 <body>
 
