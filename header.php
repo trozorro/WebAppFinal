a223fddfce358bcf10472168e86bc2d2fa6e1672
Satyajit Pradhan <satyajiit0@gmail.com>
2019-02-12 20:02:40 +0530

    major update

---
+++ header.php
@@ -0,0 +1,179 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SatyaJit
+ * Date: 12-02-2019
+ * Time: 19:59
+ */
+
+
+echo "<!DOCTYPE html>
+<html lang=\"en\">
+<head>
+    <!-- Required meta tags -->
+    <meta charset=\"utf-8\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">
+
+    <title>Trozorro - We Shape Ideas into Business</title>
+
+    <link rel=\"stylesheet\" href=\"assets/css/bootstrap.min.css\">
+
+    <link rel=\"stylesheet\" href=\"assets/css/materialdesignicons.min.css\">
+
+    <link rel=\"stylesheet\" href=\"assets/css/material.min.css\">
+
+    <link rel=\"stylesheet\" href=\"assets/css/ripples.min.css\">
+
+    <link rel=\"stylesheet\" href=\"assets/css/owl.carousel.css\">
+
+    <link rel=\"stylesheet\" href=\"assets/css/magnific-popup.css\">
+
+    <link rel=\"stylesheet\" href=\"assets/css/slicknav.css\">
+
+    <link rel=\"stylesheet\" href=\"assets/css/animate.css\">
+
+    <link rel=\"stylesheet\" href=\"assets/css/style.css\">
+
+    <link rel=\"stylesheet\" href=\"assets/css/responsive.css\">
+
+    <link rel=\"stylesheet\" href=\"assets/css/color-switcher.css\">
+
+    <link rel=\"stylesheet\" type=\"text/css\" href=\"assets/css/colors/indigo.css\" media=\"screen\" />
+
+</head>
+<body>
+
+<!-- Header Start -->
+<header id=\"header\">
+    <nav class=\"navbar navbar-expand-lg fixed-top scrolling-navbar nav-bg\">
+        <div class=\"container\">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class=\"navbar-header\">
+                <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#main-navbar\" aria-controls=\"main-navbar\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">
+                    <span class=\"sr-only\">Toggle navigation</span>
+                    <span class=\"mdi mdi-menu\"></span>
+                    <span class=\"mdi mdi-menu\"></span>
+                    <span class=\"mdi mdi-menu\"></span>
+                </button>
+                <a class=\"navbar-brand\" href=\"index.html\"><img src=\"assets/images/logo.png\" alt=\"\"></a>
+            </div>
+            <div class=\"collapse navbar-collapse\" id=\"main-navbar\">
+                <ul class=\"navbar-nav mr-auto w-100 justify-content-end\">
+
+                    <li class=\"nav-item dropdown\">
+                        <a class=\"nav-link dropdown-toggle\" href=\"#\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">
+                            Start a business
+                        </a>
+                        <div class=\"dropdown-menu\">
+                            <a class=\"dropdown-item\" href=\"about-us.html\">Private Limited Company</a>
+                            <a class=\"dropdown-item\" href=\"about-us2.html\">Limited Liability Psartnership</a>
+                            <a class=\"dropdown-item\" href=\"team.html\">One Person company</a>
+                            <a class=\"dropdown-item\" href=\"services.html\">Partnership Firm</a>
+                            <a class=\"dropdown-item\" href=\"contact-us.html\">Sole Proprietorship</a>
+
+                        </div>
+                    </li>
+                    <li class=\"nav-item dropdown\">
+                        <a class=\"nav-link dropdown-toggle\" href=\"#\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">
+                            Android Development
+                        </a>
+
+                    </li>
+                    <li class=\"nav-item dropdown\">
+                        <a class=\"nav-link dropdown-toggle\" href=\"#\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">
+                            Web Development
+                        </a>
+
+                    </li>
+                    <li class=\"nav-item dropdown\">
+                        <a class=\"nav-link dropdown-toggle\" href=\"#\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">
+                            Web Hosting
+                        </a>
+
+                    </li>
+                    <li class=\"nav-item dropdown\">
+                        <a class=\"nav-link dropdown-toggle\" href=\"#\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">
+                            Contact Us
+                        </a>
+
+                    </li>
+                </ul>
+                <!-- Search Box Start -->
+                <div class=\"search-icon\">
+              <span class=\"open-search\">
+                <i class=\"mdi mdi-magnify btn btn-common\"></i>
+              </span>
+                </div>
+                <form role=\"search\" class=\"navbar-form\">
+                    <div class=\"container\">
+                        <div class=\"row\">
+                            <div class=\"form-group has-feedback\">
+                                <input type=\"text\" placeholder=\"Type and search ...\" class=\"form-control\">
+                                <div class=\"close\"><i class=\"mdi mdi-close\"></i></div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+                <!-- Search Box End -->
+            </div>
+        </div>
+        <!-- Mobile Menu Start -->
+        <ul class=\"wpb-mobile-menu\">
+            <li>
+                <a class=\"active\" href=\"index.html\">
+                    Home
+                </a>
+            </li>
+            <li>
+                <a href=\"#\">
+                    Start a Business
+                </a>
+                <ul class=\"dropdown\">
+                    <li>
+                        <a href=\"#\">Private Limited Company</a>
+                    </li>
+                    <li>
+                        <a href=\"#\">Limited Liability Psartnership</a>
+                    </li>
+                    <li>
+                        <a href=\"#\">One Person company</a>
+                    </li>
+                    <li>
+                        <a href=\"#\">Partnership Firm</a>
+                    </li>
+                    <li>
+                        <a href=\"#\">Sole Proprietorship</a>
+                    </li>
+
+                </ul>
+            </li>
+            <li>
+                <a href=\"#\">
+                    Android Development
+                </a>
+
+            </li>
+            <li>
+                <a href=\"#\">
+                    Web Development
+                </a>
+
+            </li>
+            <li>
+                <a href=\"#\">
+                    Web Hosting
+                </a>
+
+            </li>
+            <li>
+                <a href=\"#\">
+                    Contact Us
+                </a>
+
+            </li>
+        </ul>
+        <!-- Mobile Menu End -->
+    </nav>
+</header>
+<!-- Header End -->
+";
