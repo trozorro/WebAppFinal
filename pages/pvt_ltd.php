65037b09c71674a72684ceb3e6b17d4dc608c917
Debashis Panda <panda.debasish425@gmail.com>
2019-02-15 01:28:40 +0530

    Last changes

--- pages/pvt_ltd.php
+++ pages/pvt_ltd.php
@@ -212,31 +212,31 @@ echo "<div id=\"main-slide2\" class=\"carousel slide\" data-ride=\"carousel\">
             <!-- Single About -->
             <div class=\"col-md-6 col-lg-3 col-xl-3 wow animated fadeInUp\" data-wow-delay=\".2s\">
                 <div class=\"about-single\">
-                    <i class=\"material-icons mdi mdi-pen\"></i>
-                    <h2 class=\"subtitle\">Identity Proof</h2>
-                    <p>Aadhar Card Driving License Election Id Card Passport</p>
+                    <i class=\"material-icons mdi mdi-account\"></i>
+                    <h2 class=\"subtitle\">Passport Size Photograph</h2>
+                    
                 </div>
-            </div>
-            <div class=\"col-md-6 col-lg-3 col-xl-3 wow animated fadeInUp\" data-wow-delay=\".2s\">
+            </div>";
+            echo"<div class=\"col-md-6 col-lg-3 col-xl-3 wow animated fadeInUp\" data-wow-delay=\".2s\">
                 <div class=\"about-single\">
-                    <i class=\"material-icons mdi mdi-pen\"></i>
+                    <i class=\"material-icons mdi mdi-account-card-details\"></i>
                     <h2 class=\"subtitle\">Identity Proof</h2>
-                    <p>Aadhar Card Driving License Election Id Card Passport</p>
+                    <p>Aaadhar Card,Driving License,Election Id,Card Passport</p>
                 </div>
             </div>";
             echo "<div class=\"col-md-6 col-lg-3 col-xl-3 wow animated fadeInUp\" data-wow-delay=\".2s\">
                 <div class=\"about-single\">
-                    <i class=\"material-icons mdi mdi-file-document\"></i>
-                    <h2 class=\"subtitle\">Identity Proof</h2>
-                    <p>Aadhar Card Driving License Election Id Card Passport</p>
+                    <i class=\"material-icons mdi mdi-map-marker\"></i>
+                    <h2 class=\"subtitle\">Address Proof of Business</h2>
+                    <p>Electricity Bill,Rent Agreement + NOC</p>
                 </div>
             </div>";
             
           echo "  <div class=\"col-md-6 col-lg-3 col-xl-3 wow animated fadeInUp\" data-wow-delay=\".2s\">
                 <div class=\"about-single\">
-                    <i class=\"material-icons mdi mdi-account\"></i>
-                    <h2 class=\"subtitle\">Identity Proof</h2>
-                    <p>Aadhar Card Driving License Election Id Card Passport</p>
+                    <i class=\"material-icons mdi mdi-credit-card\"></i>
+                    <h2 class=\"subtitle\">PAN Card</h2>
+                    
                 </div>
             </div>
             
@@ -247,6 +247,104 @@ echo "<div id=\"main-slide2\" class=\"carousel slide\" data-ride=\"carousel\">
            
            </div>
            </section>
+           
+           
+           <section class=\"Material-pricing-table-section section-padding\">
+<div class='container'>
+<div class=\"row\">
+<div class=\"col-md-12 wow animated fadeInLeft\" data-wow-delay=\".2s\">
+<h1 class=\"section-title\">Our Pricing</h1>
+</div>
+</div>
+<div class=\"row gg-pricing-table icon-table\">
+
+<div class=\"col-md-4 wow fadeInUp animated\" data-wow-delay=\".3s\">
+<div class=\"single-pricing-table text-center clearfix\">
+
+<div class=\"pricing-table-heading\">
+<div class=\"pricing-icon\">
+<img src=\"/assets/images/bicycle.png\" alt=\"\" class=\"center-block img-fluid\">
+</div>
+<h2>Basic</h2>
+</div>
+
+<div class=\"price\">
+<span>₹500</span>
+</div>
+
+<div class=\"pricing-item\">
+<ul>
+<li><p><strong>1</strong> User</p></li>
+<li><p><strong>Limited</strong> Projects</p></li>
+<li><p><strong>2GB</strong> Storage</p></li>
+</ul>
+</div>
+
+<div class=\"pricing-button\">
+<a href=\"javascript:void(0)\" class=\"btn btn-common\"><i class=\"fa fa-check\"></i> Buy Now<div class=\"ripple-container\"></div></a>
+</div>
+</div>
+</div>
+
+<div class=\"col-md-4 wow fadeInUp animated\" data-wow-delay=\".4s\">
+<div class=\"single-pricing-table text-center clearfix\">
+
+<div class=\"pricing-table-heading\">
+<div class=\"pricing-icon\">
+<img src=\"/assets/images/vespa-1.png\" alt=\"\" class=\"center-block img-fluid\">
+</div>
+<h2>Standard</h2>
+</div>
+
+<div class=\"price\">
+<span>₹1000</span>
+</div>
+
+<div class=\"pricing-item\">
+<ul>
+<li><p><strong>2</strong> User</p></li>
+<li><p><strong>Unlimited</strong> Projects</p></li>
+<li><p><strong>20GB</strong> Storage</p></li>
+</ul>
+</div>
+
+<div class=\"pricing-button\">
+<a href=\"javascript:void(0)\" class=\"btn btn-common\"><i class=\"fa fa-check\"></i> Buy Now<div class=\"ripple-container\"></div></a>
+</div>
+</div>
+</div>
+
+<div class=\"col-md-4 wow fadeInUp animated\" data-wow-delay=\".5s\">
+<div class=\"single-pricing-table text-center clearfix\">
+
+<div class=\"pricing-table-heading\">
+<div class=\"pricing-icon\">
+<img src=\"/assets/images/car.png\" alt=\"\" class=\"center-block img-fluid\">
+</div>
+<h2>Premium</h2>
+</div>
+
+<div class=\"price\">
+<span>₹5000</span>
+</div>
+
+<div class=\"pricing-item\">
+<ul>
+<li><p><strong>20</strong> User</p></li>
+<li><p><strong>Unlimited</strong> Projects</p></li>
+<li><p><strong>200GB</strong> Storage</p></li>
+</ul>
+</div>
+
+<div class=\"pricing-button\">
+<a href=\"javascript:void(0)\" class=\"btn btn-common\"><i class=\"fa fa-check\"></i> Buy Now<div class=\"ripple-container\"></div></a>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
 
 ";
 
