74fe60a59cb6bd0eb7f39d212c02640d323d8615
Debashis Panda <panda.debasish425@gmail.com>
2019-02-15 22:13:13 +0530

    page added

--- header.php
+++ header.php
@@ -71,8 +71,8 @@ echo "<!DOCTYPE html>
                             Start a business
                         </a>
                         <div class=\"dropdown-menu\">
-                            <a class=\"dropdown-item\" href=\"pages/pvt_ltd.php\">Private Limited Company</a>
-                            <a class=\"dropdown-item\" href=\"lmt_liability_part.php\">Limited Liability Partnership</a>
+                            <a class=\"dropdown-item\" href=\"/pages/pvt_ltd.php\">Private Limited Company</a>
+                            <a class=\"dropdown-item\" href=\"/pages/lmt_liability_part.php\">Limited Liability Partnership</a>
                             <a class=\"dropdown-item\" href=\"#\">One Person company</a>
                             <a class=\"dropdown-item\" href=\"#\">Partnership Firm</a>
                             <a class=\"dropdown-item\" href=\"#\">Sole Proprietorship</a>
