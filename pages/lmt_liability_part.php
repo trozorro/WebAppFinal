784711083b04faa57e3e225729de1b5820cace72
Debashis Panda <panda.debasish425@gmail.com>
2019-02-16 01:53:37 +0530

    Major update-Contents of page updated(FINAL UPDATE)

--- pages/lmt_liability_part.php
+++ pages/lmt_liability_part.php
@@ -28,7 +28,7 @@ echo "<div id=\"main-slide2\" class=\"carousel slide\" data-ride=\"carousel\">
 <div class=\"row\">
 
 <div class=\"col-md-12 material-title-section wow animated fadeInLeft\" data-wow-delay=\".2s\">
-<h1 class=\"section-title\">COMPANY REGISTRATION PROCESS</h1>
+<h1 class=\"section-title\">LLP A BETTER VERSION OF PARTNERSHIP FIRM</h1>
 </div>
 </div>
 <div class=\"row mt-2 wow animated fadeInUp\" data-wow-delay=\".4s\">
@@ -39,32 +39,7 @@ echo "<div id=\"main-slide2\" class=\"carousel slide\" data-ride=\"carousel\">
 
 
 <div class=\"col-md-8\">
-<p>The private limited company registration process is completely online, so you don't even need to leave your home to get your entity registered within 14 working days. At Vakilsearch, we are continuously available to help you understand how to register a private limited company. Company Registration in India can be done through VakilSearch in all major cities, including Hyderabad, Mumbai, and New Delhi, Chennai, Kolkata & all other Indian cities.</p>
-<p>A detailed explanation of the procedure now follows, along with expected timelines and charges involved.</p>
-</div>
-<div class=\"col-md-8\">
-<a href=\"#\"><i class=\"material-icons mdi mdi-bookmark-outline\"></i>Obtaining DSC and DIN</a>
-<p>Only a single director needs to have a Class-2 Digital Signature Certificate (DSC), which is needed to sign electronic documents. To get one, all you need is a self-attested copy of the director's PAN card and address proof (latest utility bill, passport, driver's license of voter's ID). This can be completed in 2 days.</p>
-</div>
-<div class=\"col-md-8\">
-<a href=\"#\"><i class=\"material-icons mdi mdi-bookmark-outline\"></i>Application for Company Name</a>
-<p>You need to now ensure that your name has not already been taken. This can be done by running a company name search. If you are disappointed that a preferred name is taken, do remember that the name of your company doesn't have to be your brand name. However, if you're going to register the brand name, also check if it has already been trademarked at http://www.ipindia.gov.in/. If it has been trademarked, you would need a no-objection certificate from its owner to have it approved as your company's name.</p>
-<p>Secondly, you need to comply with the MCA guidelines. You can do so by ensuring that the name has a unique component and a descriptive component.</p>
-</div>
-<div class=\"col-md-8\">
-<a href=\"#\"><i class=\"material-icons mdi mdi-bookmark-outline\"></i>Company Registration Form & MoA & AoA Submission</a>
-<p>Once you have your name, you need to get the Memorandum & Articles of Association approved. Here are all the other documents you'll need in order:</p>
-<p> <ul> <li>Copy of Rental Agreement (along with NOC from owner)/Sale Deed</li> <li>Latest Utility Bill</li> <li>Affidavit from director and shareholders</li> <li>Copy of PAN card, Identity and Address Proof for DIN for 2 directors</li> </ul> </p>
-<p>With these ready, you can submit the company registration form INC-32 (also known as Spice).</p>
-</div>
-<div class=\"col-md-8\">
-<a href=\"#\"><i class=\"material-icons mdi mdi-bookmark-outline\"></i>Company Incorporation Certificate</a>
-<p>Once your MoA and AoA are approved, you’re almost there. You can now get your company incorporation certificate by submitting all the above-mentioned documents to the RoC. This can take anywhere from 48 hours to even two weeks. On arrival of the company incorporation certificate, your company is registered.</p>
-</div>
-<div class=\"col-md-8\">
-<a href=\"#\"><i class=\"material-icons mdi mdi-bookmark-outline\"></i>Apply for PAN & TAN & Bank Account</a>
-<p>The moment your company is incorporated, you need to apply for a PAN & TAN with NSDL. It costs under Rs. 200, but will take up to three weeks to get done.</p>
-<p><small><strong>Note:</strong> Always be prepared for a longer wait, particularly if the Registrar of Companies (RoC) has a heavy workload at the time or your proposed company's name is rejected on first attempt.</small></p>
+<p>A Limited Liability Partnership Firm is a better version of general partnership firm carrying additional advantageous features like protection of business name, partners having limited liability shield, business listing on registrar of companies, recognition under Startup India - Standup India scheme etc. LLP represents the business legal entity more professionally among your buyer personas as compare to normal partnership firm.</p>
 </div>
 </div>
 </div>
