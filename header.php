ff3c8d9ae8be51bac77ff24dd2498a92d193db39
Suraj Kumar Jha <47494127+suraj28111999@users.noreply.github.com>
2019-02-15 23:12:02 +0530

    one person

--- header.php
+++ header.php
@@ -73,7 +73,7 @@ echo "<!DOCTYPE html>
                         <div class=\"dropdown-menu\">
                             <a class=\"dropdown-item\" href=\"/pages/pvt_ltd.php\">Private Limited Company</a>
                             <a class=\"dropdown-item\" href=\"#\">Limited Liability Partnership</a>
-                            <a class=\"dropdown-item\" href=\"/pages/1_person_comp.php\">One Person company</a>
+                            <a class=\"dropdown-item\" href=\"/pages/one_person_comp.php\">One Person company</a>
                             <a class=\"dropdown-item\" href=\"#\">Partnership Firm</a>
                             <a class=\"dropdown-item\" href=\"#\">Sole Proprietorship</a>
 
@@ -133,7 +133,7 @@ echo "<!DOCTYPE html>
                         <a href=\"#\">Limited Liability Psartnership</a>
                     </li>
                     <li>
-                        <a href=\"/pages/1_person_comp.php\">One Person company</a>
+                        <a href=\"/pages/one_person_comp.php\">One Person company</a>
                     </li>
                     <li>
                         <a href=\"#\">Partnership Firm</a>
---
+++ pages/one_person_comp.php
@@ -0,0 +1,266 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Suraj
+ * Date: 2/15/2019
+ * Time: 09:42 PM
+ */
+require_once "../config.php";
+require_once "../header.php";
+echo "<div id=\"main-slide2\" class=\"carousel slide\" data-ride=\"carousel\">
+   
+    <div class=\"carousel-inner\">
+        <div class=\"carousel-item active\">
+            <img class=\"d-block w-100\" src=\"/assets/images/slider/slider-bg1.jpg\" alt=\"First slide\">
+            <div class=\"carousel-caption d-md-block\">
+                <h1 class=\"animated wow fadeInDown hero-heading\" data-wow-delay=\".4s\">One Person Company</h1>
+                <h5 class=\"animated fadeInUp wow hero-sub-heading\" data-wow-delay=\".6s\">We help you to register your private limited company within 14 working days while you sitting at home. .</h5>
+                <!--Btns here-->
+                </div>
+        </div>
+        
+                
+                 <!--  what is  one person comoany  --> 
+                  
+                   
+        <section class=\"material-tabs-section section-padding\">
+<div class=\"container\">
+<div class=\"row\">
+
+<div class=\"col-md-12 material-title-section wow animated fadeInLeft\" data-wow-delay=\".2s\">
+<h1 class=\"section-title\">What is One Person Company (OPC) Registration?</h1>
+</div>
+</div>
+<div class=\"row mt-2 wow animated fadeInUp\" data-wow-delay=\".4s\">
+<div class=\"col-md-12\">
+<div class=\"Material-tab-default\">
+                
+                
+                
+               
+                
+             
+                
+                
+                
+           
+
+<div class=\"col-md-8\">
+The One Person Company (OPC) was recently introduced as a strong improvement over the sole proprietorship. It gives a single promoter full control over the company while limiting his/her liability to contributions to the business. This person will be the only director and shareholder (there is a nominee director, but with no power until the original director is incapable of entering into contract). So there's no chance of raising equity funding or offering employee stock options. Furthermore, if an OPC hits an average three-year turnover of over Rs. 2 crore or has a paid-up capital of over Rs. 50 lakh, it must be turned into a private limited company or public limited company within six months.<p>A detailed explanation of the procedure now follows, along with expected timelines and charges involved.</p>
+</div>
+
+</section>     
+                
+          <!--  what is  one person comoany content -->  
+          
+          
+         <!-- heiglights--->
+          
+          
+          
+          <section class=\"Material-about-section section-padding\">
+    <div class=\"container\">
+    
+    <div class=\"row\">
+            <!-- Section Title -->
+            <div class=\"col-md-12 wow animated fadeInLeft\" data-wow-delay=\".2s\">
+                <h1 class=\"section-title\">HIGHLIGHTS</h1>
+            </div>
+        </div>
+    
+        <div class=\"row\">
+            <!-- Single About -->
+            <div class=\"col-md-6 col-lg-12 col-xl-4 wow animated fadeInUp\" data-wow-delay=\".2s\">
+                <div class=\"about-single\">
+                    <i class=\"material-icons mdi mdi-pen\"></i>
+                    <h2 class=\"subtitle\">Limited Liability</h2>
+                    <p>OPC is sole proprietorship business with limited liability features.</p>
+                </div>
+            </div>
+            <!-- Single About -->
+            <div class=\"col-md-6 col-lg-12 col-xl-4 wow animated fadeInUp\" data-wow-delay=\".3s\">
+                <div class=\"about-single\">
+                    <i class=\"material-icons mdi mdi-pen\"></i>
+                    <h2 class=\"subtitle\">Single Owner</h2>
+                    <p>MThe key feature of OPC is One Person Company managed by a single owner.</p>
+                </div>
+            </div>
+            <!-- Single About -->
+            <div class=\"col-md-6 col-lg-12 col-xl-4 wow animated fadeInUp\" data-wow-delay=\".4s\">
+                <div class=\"about-single\">
+                    <i class=\"material-icons mdi mdi-pen\"></i>
+                    <h2 class=\"subtitle\">Separate Legal Entity</h2>
+                    <p>OPC is a separate legal entity from his/her owner in the eyes of law.</p>
+                </div>
+            </div>
+            <!-- Single About -->
+           
+        </div>
+        
+        
+         <div class=\"row\">
+            <!-- Single About -->
+            <div class=\"col-md-6 col-lg-12 col-xl-4 wow animated fadeInUp\" data-wow-delay=\".2s\">
+                <div class=\"about-single\">
+                    <i class=\"material-icons mdi mdi-pen\"></i>
+                    <h2 class=\"subtitle\">Share Transferability</h2>
+                    <p>OPC owner's equity cannot be transferred freely to others.</p>
+                </div>
+            </div>
+            <!-- Single About -->
+            <div class=\"col-md-6 col-lg-12 col-xl-4 wow animated fadeInUp\" data-wow-delay=\".3s\">
+                <div class=\"about-single\">
+                    <i class=\"material-icons mdi mdi-pen\"></i>
+                    <h2 class=\"subtitle\">Lesser Compliances</h2>
+                    <p>OPC requires less compliances as compare to private & public limited.</p>
+                </div>
+            </div>
+            <!-- Single About -->
+            <div class=\"col-md-6 col-lg-12 col-xl-4 wow animated fadeInUp\" data-wow-delay=\".4s\">
+                <div class=\"about-single\">
+                    <i class=\"material-icons mdi mdi-pen\"></i>
+                    <h2 class=\"subtitle\">Nomination</h2>
+                    <p>One nominee required who will take over OPC in case owner absence.</p>
+                </div>
+            </div>
+            <!-- Single About -->
+           
+        </div>
+        </section>   
+        
+        
+        
+        <!--  our process section-->
+        
+ <section  class=\"Material-about-section section-padding\">
+<div class=\"container\">
+<div class=\"row\">
+ <div class=\"col-md-12 wow animated fadeInLeft\" data-wow-delay=\".2s\">
+<h1 class=\"section-title\">Our Process</h1>
+</div>
+
+</div>
+
+<div class=\"row\">
+
+<div class=\"col-md-6 col-lg-12 col-xl-4 wow animated fadeInUp\" data-wow-delay=\".2s\">
+
+<div class=\"about-single\"><img src=\"/assets/images/ourprocess/1.png\" />
+<p>Upload the Required Documents &amp; Information to our web portal</p>
+</div>
+</div>
+
+<div class=\"col-md-6 col-lg-12 col-xl-4 wow animated fadeInUp\" data-wow-delay=\".2s\">
+<div class=\"about-single\"><img src=\"/assets/images/ourprocess/2.png\" />
+<p>Choose Package and Pay online with different payments modes available</p>
+</div>
+</div>
+
+<div class=\"col-md-6 col-lg-12 col-xl-4 wow animated fadeInUp\" data-wow-delay=\".2s\">
+<div class=\"about-single\"><img src=\"/assets/images/ourprocess/3.png\" />
+<p>We will apply for Digital Signature and Director Identification Number of proposed directors</p>
+</div>
+</div>
+
+<div class=\"col-md-6 col-lg-12 col-xl-4 wow animated fadeInUp\" data-wow-delay=\".2s\">
+<div class=\"about-single\"><img src=\"/assets/images/ourprocess/4.png\" />
+<p>Check for Company&rsquo;s Name availability, apply for Name Approval.</p>
+</div>
+</div>
+
+<div class=\"col-md-6 col-lg-12 col-xl-4 wow animated fadeInUp\" data-wow-delay=\".2s\">
+<div class=\"about-single\"><img src=\"/assets/images/ourprocess/5.png\" />
+<p>Prepare MOA, AOA and other necessary incorporation documents.</p>
+</div>
+</div>
+
+<div class=\"col-md-6 col-lg-12 col-xl-4 wow animated fadeInUp\" data-wow-delay=\".2s\">
+<div class=\"about-single\"><img src=\"/assets/images/ourprocess/6.png\" />
+<p>File incorporation documents &amp; obtain Certificate of Incorporation, PAN &amp; TAN.</p>
+</div>
+</div>
+
+
+</div>
+
+</div>
+</div>
+</div>
+</section>
+
+
+          
+<section class=\"Material-about-section section-padding\">
+    <div class=\"container\">
+    
+    <div class=\"row\">
+            <!-- Section Title -->
+            <div class=\"col-md-12 wow animated fadeInLeft\" data-wow-delay=\".2s\">
+                <h1 class=\"section-title\">DOCUMENTS REQUIRED</h1>
+            </div>
+        </div>
+    
+        <div class=\"row\">
+            <!-- Single About -->
+     
+          <div class=\"col-md-6 col-lg-3 col-xl-3 wow animated fadeInUp\" data-wow-delay=\".2s\">
+                <div class=\"about-single\">
+                    <i class=\"material-icons mdi mdi-account-card-details\"></i>
+                    <h2 class=\"subtitle\">Identity Proof</h2>
+                    <p>Aaadhar Card , Driving License , Election Id , Card Passport</p>
+                </div>
+            </div>
+       
+
+
+<div class=\"col-md-6 col-lg-3 col-xl-3 wow animated fadeInUp\" data-wow-delay=\".2s\">
+                <div class=\"about-single\">
+                    <i class=\"material-icons mdi mdi-account\"></i>
+                    <h2 class=\"subtitle\">Passport Size Photograph</h2>
+                    
+                </div>
+            </div>
+
+
+<div class=\"col-md-6 col-lg-3 col-xl-3 wow animated fadeInUp\" data-wow-delay=\".2s\">
+                <div class=\"about-single\">
+                    <i class=\"material-icons mdi mdi-map-marker\"></i>
+                    <h2 class=\"subtitle\">Address Proof of Business</h2>
+                    <p>Electricity Bill , Rent Agreement + NOC</p>
+                </div>
+            </div>
+            
+     
+  
+  
+  
+  
+  <div class=\"col-md-6 col-lg-3 col-xl-3 wow animated fadeInUp\" data-wow-delay=\".2s\">
+                <div class=\"about-single\">
+                    <i class=\"material-icons mdi mdi-file-document-box\"></i>
+                    <h2 class=\"subtitle\">PAN Card</h2>
+                    
+                </div>
+            </div>
+            
+            
+            
+            
+           </div> 
+           
+           </div>
+           </section>
+                   
+                
+";
+
+
+
+
+
+require_once "../faq_pages/pvt_ltd_faq.php";
+
+require_once "../page_elements/compare_design.php";
+
+require_once "../page_elements/contact_form_design.php";
+require_once "../footer.php";
