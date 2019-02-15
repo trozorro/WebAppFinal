139ea0d0c1da6db97f4f229da7427bd9e8fbaccd
Suraj Kumar Jha <47494127+suraj28111999@users.noreply.github.com>
2019-02-15 21:54:43 +0530

    one person

--- pages/1_person_comp.php
+++ pages/1_person_comp.php
@@ -5,3 +5,31 @@
  * Date: 2/15/2019
  * Time: 09:42 PM
  */
+require_once "../config.php";
+require_once "../header.php";
+echo"<div id=\"main-slide2\" class=\"carousel slide\" data-ride=\"carousel\">
+   
+    <div class=\"carousel-inner\">
+        <div class=\"carousel-item active\">
+            <img class=\"d-block w-100\" src=\"/assets/images/slider/slider-bg1.jpg\" alt=\"First slide\">
+            <div class=\"carousel-caption d-md-block\">
+                <h1 class=\"animated wow fadeInDown hero-heading\" data-wow-delay=\".4s\">Private Limited Company</h1>
+                <h5 class=\"animated fadeInUp wow hero-sub-heading\" data-wow-delay=\".6s\">We help you to register your private limited company within 14 working days while you sitting at home. .</h5>
+                <!--Btns here-->
+                </div> 
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+";
+require_once "../footer.php";
