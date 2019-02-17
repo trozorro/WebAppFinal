ab658af2849579c7df4ddb7920e3779072db4ca0
Debashis Panda <panda.debasish425@gmail.com>
2019-02-17 16:38:50 +0530

    All pages added.

--- home_elements/services_cards.php
+++ home_elements/services_cards.php
@@ -19,7 +19,7 @@ echo "<section class=\"Material-about-section section-padding\">
     
         <div class=\"row\">
             <!-- Single About -->
-            <div class=\"col-md-6 col-lg-3 col-xl-3 wow animated fadeInUp\" data-wow-delay=\".2s\">
+            <div class=\"col-md-6 col-lg-12 col-xl-4 wow animated fadeInUp\" data-wow-delay=\".2s\">
                 <div class=\"about-single\">
                     <i class=\"material-icons mdi mdi-pen\"></i>
                     <h2 class=\"subtitle\">COMPANY INCORPORATION</h2>
@@ -27,7 +27,7 @@ echo "<section class=\"Material-about-section section-padding\">
                 </div>
             </div>
             <!-- Single About -->
-            <div class=\"col-md-6 col-lg-3 col-xl-3 wow animated fadeInUp\" data-wow-delay=\".3s\">
+            <div class=\"col-md-6 col-lg-12 col-xl-4 wow animated fadeInUp\" data-wow-delay=\".3s\">
                 <div class=\"about-single\">
                     <i class=\"material-icons mdi mdi-cash-multiple\"></i>
                     <h2 class=\"subtitle\">GST</h2>
@@ -36,17 +36,9 @@ echo "<section class=\"Material-about-section section-padding\">
                 </div>
             </div>
             <!-- Single About -->
-            <div class=\"col-md-6 col-lg-3 col-xl-3 wow animated fadeInUp\" data-wow-delay=\".4s\">
-                <div class=\"about-single\">
-                    <i class=\"material-icons mdi mdi-certificate\"></i>
-                    <h2 class=\"subtitle\">ISO CERTIFICATION</h2>
-                    <p>ISO certification process is the key to achieve the prime target of client satisfaction and overall excellence of your business as per the global quality standards.
-
-</p>
-                </div>
-            </div>
+           
             <!-- Single About -->
-            <div class=\"col-md-6 col-lg-3 col-xl-3 wow animated fadeInUp\" data-wow-delay=\".5s\">
+            <div class=\"col-md-6 col-lg-12 col-xl-4 wow animated fadeInUp\" data-wow-delay=\".5s\">
                 <div class=\"about-single\">
                     <i class=\"material-icons mdi mdi-check-circle\"></i>
                     <h2 class=\"subtitle\">TRADEMARK</h2>
@@ -57,26 +49,20 @@ echo "<section class=\"Material-about-section section-padding\">
         
         
          <div class=\"row\">
+            <!-- ingle About -->
+           <div class=\"row\">
             <!-- Single About -->
-            <div class=\"col-md-6 col-lg-3 col-xl-3 wow animated fadeInUp\" data-wow-delay=\".2s\">
+            <div class=\"col-md-6 col-lg-12 col-xl-4 wow animated fadeInUp\" data-wow-delay=\".2s\">
                 <div class=\"about-single\">
-                    <i class=\"material-icons mdi mdi-account\"></i>
-                    <h2 class=\"subtitle\">PATENT</h2>
-                    <p>Patent is an intellectual property one can protect their original invention from infringement by applying patent. Our professional can help you in patent registration online.
+                    <i class=\"material-icons mdi mdi-layers\"></i>
+                    <h2 class=\"subtitle\">FSSAI REGISTRATION</h2>
+                    <p>Food security is the prime concern for government & consumers. With FSSAI registration, you can run your food business in compliance with standards of quality and health safety.
 
 </p>
                 </div>
             </div>
-            <!-- Single About -->
-            <div class=\"col-md-6 col-lg-3 col-xl-3 wow animated fadeInUp\" data-wow-delay=\".3s\">
-                <div class=\"about-single\">
-                    <i class=\"material-icons mdi mdi-copyright\"></i>
-                    <h2 class=\"subtitle\">COPYRIGHT</h2>
-                    <p>Copyright registration helps protect your original artistic, literary and creative works. Through our online process, you can protect your unique creation from plagiarism.</p>
-                </div>
-            </div>
-            <!-- Single About -->
-            <div class=\"col-md-6 col-lg-3 col-xl-3 wow animated fadeInUp\" data-wow-delay=\".4s\">
+           
+            <div class=\"col-md-6 col-lg-12 col-xl-4 wow animated fadeInUp\" data-wow-delay=\".4s\">
                 <div class=\"about-single\">
                     <i class=\"material-icons mdi mdi-arrow-all\"></i>
                     <h2 class=\"subtitle\">IEC CODE</h2>
@@ -84,7 +70,7 @@ echo "<section class=\"Material-about-section section-padding\">
                 </div>
             </div>
             <!-- Single About -->
-            <div class=\"col-md-6 col-lg-3 col-xl-3 wow animated fadeInUp\" data-wow-delay=\".5s\">
+            <div class=\"col-md-6 col-lg-12 col-xl-4 wow animated fadeInUp\" data-wow-delay=\".5s\">
                 <div class=\"about-single\">
                     <i class=\"material-icons mdi mdi-newspaper\"></i>
                     <h2 class=\"subtitle\">BUSINESS PLAN AND PROJECT REPORT</h2>
@@ -96,19 +82,9 @@ echo "<section class=\"Material-about-section section-padding\">
         </div>
        
        
-        <div class=\"row\">
+        
             <!-- Single About -->
-            <div class=\"col-md-6 col-lg-3 col-xl-3 wow animated fadeInUp\" data-wow-delay=\".2s\">
-                <div class=\"about-single\">
-                    <i class=\"material-icons mdi mdi-layers\"></i>
-                    <h2 class=\"subtitle\">FSSAI REGISTRATION</h2>
-                    <p>Food security is the prime concern for government & consumers. With FSSAI registration, you can run your food business in compliance with standards of quality and health safety.
-
-</p>
-                </div>
-            </div>
-            <!-- Single About -->
-            <div class=\"col-md-6 col-lg-3 col-xl-3 wow animated fadeInUp\" data-wow-delay=\".3s\">
+            <div class=\"col-md-6 col-lg-12 col-xl-4 wow animated fadeInUp\" data-wow-delay=\".3s\">
                 <div class=\"about-single\">
                     <i class=\"material-icons mdi mdi-android\"></i>
                     <h2 class=\"subtitle\">ANDROID DEVELOPMENT</h2>
@@ -116,7 +92,7 @@ echo "<section class=\"Material-about-section section-padding\">
                 </div>
             </div>
             <!-- Single About -->
-            <div class=\"col-md-6 col-lg-3 col-xl-3 wow animated fadeInUp\" data-wow-delay=\".4s\">
+            <div class=\"col-md-6 col-lg-12 col-xl-4 wow animated fadeInUp\" data-wow-delay=\".4s\">
                 <div class=\"about-single\">
                     <i class=\"material-icons mdi mdi-web\"></i>
                     <h2 class=\"subtitle\">WEB DEVELOPMENT</h2>
@@ -124,7 +100,7 @@ echo "<section class=\"Material-about-section section-padding\">
                 </div>
             </div>
             <!-- Single About -->
-            <div class=\"col-md-6 col-lg-3 col-xl-3 wow animated fadeInUp\" data-wow-delay=\".5s\">
+            <div class=\"col-md-6 col-lg-12 col-xl-4 wow animated fadeInUp\" data-wow-delay=\".5s\">
                 <div class=\"about-single\">
                     <i class=\"material-icons mdi mdi-responsive\"></i>
                     <h2 class=\"subtitle\">WEB HOSTING</h2>
